f8ff843f54bf93a069740ca5e3aed773e2329e6b
Danya <kurdukovdanila234@gmail.com>
2022-10-02 22:45:03 +0300

    Add CameraDevice.StateCallback

--- app/src/main/java/com/example/gpscameraapp/MainActivity.java
+++ app/src/main/java/com/example/gpscameraapp/MainActivity.java
@@ -1,22 +1,45 @@
 package com.example.gpscameraapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int PERMISSIONS_REQUEST_CODE = 100;
 
-    
+    CameraManager cameraManager;
+
+    private TextureView previewForm;
+    private Button btnTakePicture;
+
+    private String cameraLensFacing = "0";
+
+    private CameraDevice cameraDevice;
+
+    private CameraCaptureSession cameraCaptureSession;
+
+    private CaptureRequest.Builder builder;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        previewForm = findViewById(R.id.textureView);
+        btnTakePicture = findViewById(R.id.button_take_picture);
     }
 
     @Override
@@ -29,6 +52,34 @@ public class MainActivity extends AppCompatActivity {
             else requestPermissions(new String[] {Manifest.permission.CAMERA},PERMISSIONS_REQUEST_CODE);
         }
         else Toast.makeText(getApplicationContext(), "Sorry, Camera feature is necessary!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            createCameraPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+
+        }
+    }
+
+    private void createCameraPreview() {
+    }
+
+
+    private void finishCamera(){
+        cameraDevice.close();
+        cameraDevice = null;
     }
 
     private boolean hasFeatureCamera(){
