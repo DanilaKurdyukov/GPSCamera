f7af5e1b4c62e247e52d7ee0cf6f6452e5586f0a
Danya <kurdukovdanila234@gmail.com>
2022-10-02 22:35:28 +0300

    Split methods for permission and device has Camera

--- app/src/main/java/com/example/gpscameraapp/MainActivity.java
+++ app/src/main/java/com/example/gpscameraapp/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int PERMISSIONS_REQUEST_CODE = 100;
 
-
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private boolean hasFeatureAndPermissions(){
-        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)
-                && checkSelfPermission(Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED){
-            return true;
-        }
-        else return false;
-    }
-    private void requestPermissions(){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (hasFeatureCamera()){
+            if (isPermissionGranted()){
 
+            }
+            else requestPermissions(new String[] {Manifest.permission.CAMERA},PERMISSIONS_REQUEST_CODE);
+        }
+        else Toast.makeText(getApplicationContext(), "Sorry, Camera feature is necessary!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean hasFeatureCamera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) ? true : false;
+    }
+
+    private boolean isPermissionGranted(){
+        return checkSelfPermission(Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED ? true : false;
     }
 }
