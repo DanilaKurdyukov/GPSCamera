6844b84fd00d7090b9b9688603d7e028cbc37c3b
Danya <kurdukovdanila234@gmail.com>
2022-10-02 22:26:26 +0300

    Add method for check if device has camera or not and check if permissions granted

--- app/src/main/java/com/example/gpscameraapp/MainActivity.java
+++ app/src/main/java/com/example/gpscameraapp/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.gpscameraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int PERMISSIONS_REQUEST_CODE = 100;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private boolean hasFeatureAndPermissions(){
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)
+                && checkSelfPermission(Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else return false;
+    }
+    private void requestPermissions(){
+
     }
 }
