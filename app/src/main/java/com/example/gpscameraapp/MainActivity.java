3b7cbd392fda632239a0cf915eadcbe8c719f644
Danya <kurdukovdanila234@gmail.com>
2022-10-03 16:11:11 +0300

    Add method chooseOptimalSize. When orientaion landscapecamera preview is stretched

--- app/src/main/java/com/example/gpscameraapp/MainActivity.java
+++ app/src/main/java/com/example/gpscameraapp/MainActivity.java
@@ -10,18 +10,25 @@ import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
     private CaptureRequest.Builder builder;
 
-
+    private Size previewSize;
+    private Size[] imageDimension;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +89,12 @@ public class MainActivity extends AppCompatActivity {
         SurfaceTexture surfaceTexture = previewForm.getSurfaceTexture();
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        surfaceTexture.setDefaultBufferSize(metrics.widthPixels*2,metrics.heightPixels*2);
+        int screenWidth = metrics.widthPixels;
+        int screenHeight = metrics.heightPixels / 2;
+        Size sizeFinish = chooseOptimalSize(imageDimension, screenWidth, screenHeight);
+        int cameraSizeW = sizeFinish.getWidth();
+        int cameraSizeH = sizeFinish.getHeight();
+        surfaceTexture.setDefaultBufferSize(cameraSizeW, cameraSizeH);
         Surface surface = new Surface(surfaceTexture);
         builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
         builder.addTarget(surface);
@@ -131,6 +144,9 @@ public class MainActivity extends AppCompatActivity {
     private void startCamera(String cameraLensFacing) {
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
+            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraLensFacing);
+            StreamConfigurationMap map =characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            imageDimension = map.getOutputSizes(SurfaceTexture.class);
             cameraManager.openCamera(cameraLensFacing,stateCallback,null);
         } catch (CameraAccessException e) {
             e.printStackTrace();
@@ -160,6 +176,40 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         finishCamera();
     }
+
+
+    private Size chooseOptimalSize(Size[] sizes, int w, int h){
+        final double ASPECT_TOLERANCE = 1;
+        double targetRatio = (double) h / w;
+
+        if (sizes == null) return null;
+        Size optimalSize = null;
+
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Size size : sizes) {
+            double ratio = (double) size.getWidth() / size.getHeight();
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.getHeight() - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.getHeight() - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Size size : sizes) {
+                if (Math.abs(size.getHeight() - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.getHeight() - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+
 
     private void finishCamera(){
         cameraDevice.close();
